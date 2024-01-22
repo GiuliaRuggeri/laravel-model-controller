c40fb42707322b7c5e91fa5ac757e0b68375de3b
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-01-22 16:20:55 +0100

    esercizio completo + bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+
+<h1 class="text-center fw-bold mt-3 title">Movies</h1>
+<div class="container">
+  <div class="row">
+    @foreach ($movies as $movie)
+    <div class="col-4 mt-3 mb-3">
+        <div class="card opacity-75 text-center">
+            <div class="card-header fw-bold cardtitle">
+                {{$movie->title}}
+            </div>
+            <div class="card-body cardbody">
+              <p><span class="fw-bold">Original title:</span> {{$movie->original_title}}</p>
+              <p><span class="fw-bold">Nationality: </span>{{$movie->nationality}}</p>
+              <p><span class="fw-bold">Date:</span> {{$movie->date}}</p>
+              <p><span class="fw-bold">Vote: </span>{{$movie->vote}}</p>
+
+
+            </div>
+          </div>
+
+    </div>
+
+
+@endforeach
+
+</div>  
+</div>
+
+
+
 @endsection
